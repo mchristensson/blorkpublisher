05da6d97062dc5e7d5dc18d71433ccd7d9ee51ff
Magnus Christensson <m.e.christensson@icloud.com>
2022-11-15 10:46:48 +0100

    Initial Commit

---
+++ QueueManagerLab1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QueueManagerLab1
+//
+//  Created by Magnus Christensson on 2022-11-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QueueManagerLab1/QueueManagerLab1App.swift
@@ -0,0 +1,17 @@
+//
+//  QueueManagerLab1App.swift
+//  QueueManagerLab1
+//
+//  Created by Magnus Christensson on 2022-11-15.
+//
+
+import SwiftUI
+
+@main
+struct QueueManagerLab1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
